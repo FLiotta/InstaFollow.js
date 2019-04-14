b0d0ff6224241dfed848f72244d4595501b467e2
Fabrizio Liotta <32228156+FLiotta@users.noreply.github.com>
2019-04-14 18:57:42 -0300

    Fixed Typo

--- InstaFollow.js
+++ InstaFollow.js
@@ -5,7 +5,7 @@ let followersButtons = [...document.querySelectorAll('li > div > div > button')]
 setInterval(() => {
 	if(followersButtons.length > 0){
 		if(followersButtons[0].textContent == "Follow")
-			button.click();
+			followersButtons[0].click();
 		else
 			console.log("There were an error following this guy.");
 		followersDiv.scrollTop += 50;
